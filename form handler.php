3cfd4dc6edf68a80e5e3aa1a14123614f2591bb2
kedevelopr <kevindeveloper078@gmail.com>
2025-06-19 13:26:25 +0300

    Add files via upload

---
+++ form handler.php
@@ -0,0 +1,28 @@
+<?php
+$name = $_post['name'];
+$visitor_email = $_post['email'];
+$subject = $_post['subject'];
+$message = $_post['message'];
+
+
+$email_from = 'info@olmcmaryhillgirls.com';
+
+$email_subject = 'new form submission';
+
+$email_body = "user name: $name.\n".
+                "user email:$visitor_email.\n".
+                "subject: $subject.\n".
+                "user message: $message .\n";
+
+ $to = 'marhillgirls@yahoo.com'; 
+ 
+ $headers = "From: $email_from \r\n";
+
+ $headers .= "Reply-To: $visitor_email \r\n";
+
+ mail($to,email_subject,$email_body, $headers);
+
+ header("location: index.html");
+                
+
+?>
